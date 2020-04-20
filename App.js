c189b81966ce0798ce60e1eabccdd98a6451ac13
Brian Bouchard <b.bouch@gmail.com>
2020-04-20 13:59:48 -0600

    install react navigation and dependencies, create navigation folder, create mealsNavigator component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
