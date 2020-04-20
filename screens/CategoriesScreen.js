fb64dbd670315cfa6aef7688e27f3bad7cee9bdc
Brian Bouchard <b.bouch@gmail.com>
2020-04-20 12:50:56 -0600

    create shell CategoriesScreen component

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import react from 'react';
+import {} from 'react-native';
+
+const CategoriesScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Categories Screen!</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default CategoriesScreen;
