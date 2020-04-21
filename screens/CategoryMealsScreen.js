593e94569d08804135c75f31cc66b9cb02612d98
Brian Bouchard <b.bouch@gmail.com>
2020-04-20 21:12:00 -0600

    add button and route navigation from category meals screen to meal detail screen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 const CategoryMealScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
+      <Text>The Meal Screen!</Text>
+      <Button title="Go to Meal Detail!" onPress={() => {
+        props.navigation.navigate({routeName: 'MealDetail'})
+      }} />
     </View>
   )
 };
