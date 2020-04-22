b079a9b9db18d49d33e43f948d41cb6b4b59089a
Brian Bouchard <b.bouch@gmail.com>
2020-04-21 21:32:42 -0600

    add grid view to categories screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -4,7 +4,11 @@ import { StyleSheet, View, Text, FlatList } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
 
 const renderGridItem = (itemData) => {
-  return <View><Text>{itemData.item.title}</Text></View>
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  )
 }
 
 const CategoriesScreen = (props) => {
@@ -24,6 +28,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   }
 });
 
