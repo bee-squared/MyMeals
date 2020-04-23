f42cc5d7d88053f7a678a4c404d4f0bcc78a5cb5
Brian Bouchard <b.bouch@gmail.com>
2020-04-23 12:43:21 -0600

    add touchable native feedback for android, add styling to categories

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, Text, StyleSheet, Platform,TouchableNativeFeedback } from 'react-native';
 
 const CategoryGridTile = (props) => {
+  let TouchableCmp = TouchableOpacity;
+
+  // provides ripple effect on android onPress()
+  if (Platform.OS === 'android' &&  Platform.Version >= 21) {
+    TouchableCmp = TouchableNativeFeedback
+  }
+
   return (
-    <TouchableOpacity
-    style={styles.gridItem}
-    onPress={props.onSelect}
-  >
-    <View>
-      <Text>{props.title}</Text>
+    <View style={styles.gridItem}>
+      <TouchableCmp style={{flex: 1}} onPress={props.onSelect}>
+        <View style={{...styles.container, ...{backgroundColor: props.color}}}>
+          <Text style={styles.title} numberOfLines={2}>{props.title}</Text>
+        </View>
+      </TouchableCmp>
     </View>
-  </TouchableOpacity>
   )
 };
 
@@ -20,6 +26,23 @@ const styles = StyleSheet.create({
     margin: 15,
     height: 150,
   },
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
+    elevation: 3, // for android
+    padding: 15,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    textAlign: 'right',
+  }
 });
 
 export default CategoryGridTile;
