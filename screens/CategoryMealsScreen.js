342d9cb0e427078261c47ded18c9435bf750a293
Brian Bouchard <b.bouch@gmail.com>
2020-04-24 09:15:03 -0600

    install prettier and format MealItem in CategoryMealScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { FlatList, StyleSheet, View, Text } from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  View,
+  Text,
+  ImageBackgroundComponent,
+} from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
 import MealItem from '../components/MealItem';
 
 const CategoryMealScreen = (props) => {
-
   const renderMealItem = (itemData) => {
     return (
       <MealItem
@@ -13,14 +18,15 @@ const CategoryMealScreen = (props) => {
         duration={itemData.item.duration}
         complexity={itemData.item.complexity.toUpperCase()}
         affordability={itemData.item.affordability.toUpperCase()}
-        onSelectMeal={() => {}}/>
-    )
-  }
+        onSelectMeal={() => {}}
+      />
+    );
+  };
 
   const catId = props.navigation.getParam('categoryId');
 
   const displayedMeals = MEALS.filter((meal) => {
-    return meal.categoryIds.indexOf(catId) >= 0
+    return meal.categoryIds.indexOf(catId) >= 0;
   });
 
   return (
@@ -29,27 +35,27 @@ const CategoryMealScreen = (props) => {
         data={displayedMeals}
         keyExtractor={(item, index) => item.id}
         renderItem={renderMealItem}
-        style={{width: '100%'}}
+        style={{ width: '100%' }}
       />
     </View>
-  )
+  );
 };
 
 CategoryMealScreen.navigationOptions = (navigationData) => {
-  const catId = navigationData.navigation.getParam('categoryId')
-  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId)
+  const catId = navigationData.navigation.getParam('categoryId');
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
   return {
     headerTitle: selectedCategory.title,
-  }
-}
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
 
 export default CategoryMealScreen;
