ececd0aba23aa150d5ed7a967e1c67247ff27d65
Brian Bouchard <b.bouch@gmail.com>
2020-04-24 12:40:58 -0600

    add icon to header, still requires a proper function onPress

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,37 +1,46 @@
 import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
       <Text>{selectedMeal.title}</Text>
-      <Button title="Go Back to Categories" onPress={() => {
-        props.navigation.popToTop();
-      }} />
+      <Button
+        title='Go Back to Categories'
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
     </View>
-  )
+  );
 };
 
 // add name to header
 MealDetailScreen.navigationOptions = (naviationData) => {
   const mealId = naviationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return {
     headerTitle: selectedMeal.title,
-    headerRight: () => <Text>FAVS!</Text>,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Favorite' iconName='ios-star' onPress={() => console.log('Mark as Favorite!')} />
+      </HeaderButtons>
+    ),
   };
-}
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
 
 export default MealDetailScreen;
