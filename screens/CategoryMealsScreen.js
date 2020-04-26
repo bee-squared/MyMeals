edd822821376899a8c36e441c5e929b7c8aec765
Brian Bouchard <b.bouch@gmail.com>
2020-04-26 14:59:30 -0600

    create reusable MealList component, render MealList on CategoryMealsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import { FlatList, StyleSheet, View, Text } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
-import MealItem from '../components/MealItem';
+import MealList from '../components/MealList';
 
 const CategoryMealScreen = (props) => {
-
-  const renderMealItem = (itemData) => {
-
-    const onSelectMeal = () => {
-      props.navigation.navigate({
-        routeName: 'MealDetail',
-        params: {
-          categoryId: itemData.item.id,
-        },
-      });
-    };
-
-    return (
-      <MealItem
-        title={itemData.item.title}
-        image={itemData.item.imageUrl}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity.toUpperCase()}
-        affordability={itemData.item.affordability.toUpperCase()}
-        onSelectMeal={() => {
-          return props.navigation.navigate({
-            routeName: 'MealDetail',
-            params: {
-              mealId: itemData.item.id,
-            },
-          });
-        }}
-      />
-    );
-  };
 
   const catId = props.navigation.getParam('categoryId');
 
@@ -43,14 +12,7 @@ const CategoryMealScreen = (props) => {
   });
 
   return (
-    <View style={styles.screen}>
-      <FlatList
-        data={displayedMeals}
-        keyExtractor={(item, index) => item.id}
-        renderItem={renderMealItem}
-        style={{ width: '90%' }}
-      />
-    </View>
+    <MealList listData={displayedMeals} navigation={props.navigation}/>
   );
 };
 
@@ -62,13 +24,5 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
 
 export default CategoryMealScreen;
