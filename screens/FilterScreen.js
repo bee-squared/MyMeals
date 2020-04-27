7f720965958cdc2e4bb2218486933eed05ca25c8
Brian Bouchard <b.bouch@gmail.com>
2020-04-27 10:44:44 -0600

    create FilterSwitch, refactor switch, add switches for filter options

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -4,22 +4,51 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
 import Colors from '../constants/Colors';
+import { set } from 'react-native-reanimated';
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterContainer}>
+      <Text>{props.label}</Text>
+      <Switch
+        trackColor={{ true: Colors.primaryColor }}
+        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+        value={props.state}
+        onValueChange={props.onChange}
+      />
+    </View>
+  );
+};
 
 const FiltersScreen = (props) => {
   const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
 
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Available Filters / Restrictions</Text>
-      <View style={styles.filterContainer}>
-        <Text>Gluten-Free</Text>
-        <Switch
-          trackColor={{true: Colors.primaryColor}}
-          thumbColor={Platform.OS === 'android' ? Colors.primaryColor : 'white'}
-          value={isGlutenFree}
-          onValueChange={(newValue) => setIsGlutenFree(newValue)}
-        />
-      </View>
+      <FilterSwitch
+        label='Gluten-Free'
+        state={isGlutenFree}
+        onChange={(newValue) => setIsGlutenFree(newValue)}
+      />
+      <FilterSwitch
+        label='Lactose-Free'
+        state={isLactoseFree}
+        onChange={(newValue) => setIsLactoseFree(newValue)}
+      />
+      <FilterSwitch
+        label='Vegan'
+        state={isVegan}
+        onChange={(newValue) => setIsVegan(newValue)}
+      />
+      <FilterSwitch
+        label='Vegetarian'
+        state={isVegetarian}
+        onChange={(newValue) => setIsVegetarian(newValue)}
+      />
     </View>
   );
 };
@@ -57,6 +86,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     width: '80%',
+    marginVertical: 15,
   },
 });
 
