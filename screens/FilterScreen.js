3974720583581e52c940252deae66ca66a7b080a
Brian Bouchard <b.bouch@gmail.com>
2020-04-27 10:12:49 -0600

    add Switch to filter screen, style filter screen

--- screens/FilterScreen.js
+++ screens/FilterScreen.js
@@ -1,15 +1,21 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Switch } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
 
 const FiltersScreen = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screen!</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <View style={styles.filterContainer}>
+        <Text>Gluten-Free</Text>
+        <Switch value={isGlutenFree} onValueChange={newValue => setIsGlutenFree(newValue)} />
+      </View>
     </View>
-  )
+  );
 };
 
 FiltersScreen.navigationOptions = (navData) => {
@@ -17,20 +23,35 @@ FiltersScreen.navigationOptions = (navData) => {
     headerTitle: 'Filter Meals',
     headerLeft: (
       <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item title='Menu' iconName='ios-menu' onPress={()=>{
-          navData.navigation.toggleDrawer();
-        }} />
+        <Item
+          title='Menu'
+          iconName='ios-menu'
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
       </HeaderButtons>
-    )
-  }
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 22,
+    margin: 20,
+    textAlign: 'center',
+  },
+  filterContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '80%',
+  },
 });
 
 export default FiltersScreen;
