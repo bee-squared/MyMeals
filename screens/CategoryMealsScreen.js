2298b40abfebbd6b2e26449152185ef8bfa0ed65
Brian Bouchard <b.bouch@gmail.com>
2020-05-07 10:35:27 -0600

    Add empty state to meals list when filters selected render zero meals

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import { View, StyleSheet } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealScreen = (props) => {
-
   const catId = props.navigation.getParam('categoryId');
 
-  const availableMeals = useSelector(state => state.meals.filteredMeals);
+  const availableMeals = useSelector((state) => state.meals.filteredMeals);
 
   const displayedMeals = availableMeals.filter((meal) => {
     return meal.categoryIds.indexOf(catId) >= 0;
   });
 
-  return (
-    <MealList listData={displayedMeals} navigation={props.navigation}/>
-  );
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found, check your filters.</DefaultText>
+      </View>
+    );
+  }
+
+  return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
 
 CategoryMealScreen.navigationOptions = (navigationData) => {
@@ -27,5 +34,13 @@ CategoryMealScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title,
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default CategoryMealScreen;
